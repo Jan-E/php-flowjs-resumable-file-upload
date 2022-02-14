1027d2b1c41171a6f2a25de36a2b88a7fe583c85
Jan Ehrhardt <github@ehrhardt.nl>
2022-02-14 22:54:38 +0100

    Add uploads/show.php with uuid, uid, sesnid

--- uploads/show.php
+++ uploads/show.php
@@ -2,7 +2,7 @@
 $uid	= isset($_REQUEST['uid']) ? intval($_REQUEST['uid']) : 0;
 $uuid	= isset($_REQUEST['uuid']) ? stripslashes($_REQUEST['uuid']) : '';
 $sesnid	= isset($_REQUEST['sesnid']) ? intval($_REQUEST['sesnid']) : 0;
-$show   = isset($_REQUEST['output']) ? stripslashes($_REQUEST['show']) : '';
+$show   = isset($_REQUEST['output']) ? stripslashes($_REQUEST['output']) : '';
 $output = "pk000002.opt.mp4";
 copy($show,$output);
 ?><!DOCTYPE html>
