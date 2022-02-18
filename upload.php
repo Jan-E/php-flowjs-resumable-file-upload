59be5b80c0b81aa5982037895c731f09f23e5649
Jan Ehrhardt <github@ehrhardt.nl>
2022-02-18 03:37:37 +0100

    Be patient after 95% upload - take 4

--- upload.php
+++ upload.php
@@ -18,9 +18,8 @@ set_time_limit(3600); // 1 hour max_execution_time
 						<!-- Upload button -->
 						<button type="button" id="upbrowse" class="btn btn-primary"><span class="glyphicon glyphicon-upload"></span> Browse Files</button>
 						<button type="button" id="upToggle" class="btn btn-default"><span class="glyphicon glyphicon-play"></span> Pause OR Continue</button>
-					
-						<div id="upPatient" class="col-sm-8 col-sm-offset-2 text-center">After the file is uploaded we have to knit all chunks. This may take a couple of minutes. Please be patient when the upload is over 95%.<br /></div>
-						<div col-sm-12 text-center"><br /></div>
+					</p>
+					<p>
 						<!-- Upload file listing -->
 						<div id="uplist"></div>
 					</p>
