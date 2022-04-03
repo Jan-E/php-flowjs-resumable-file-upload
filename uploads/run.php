19e6f60a528874e02263f5bb937cc00d7f5ae912
Jan Ehrhardt <github@ehrhardt.nl>
2022-04-03 14:45:27 +0200

    Fix rotated videos (from my Sony Xperia XZ2 Compact)

--- uploads/run.php
+++ uploads/run.php
@@ -9,6 +9,30 @@ if (isset($_REQUEST['input'])) {
 	if ($max <= 448) $crf = 26;
 	$crf = isset($_REQUEST['crf']) && intval($_REQUEST['crf']) ? intval($_REQUEST['crf']) : $crf;
 	$scale = 'scale='.$max.':-2,setsar=1:1';
+	$rotated = false;
+	@exec("mediainfo $input 2>nul", $str);
+	while (list($nr,$text) = each($str)) {
+		/*	deal with rotated videos (from my Sony Xperia XZ2 Compact)
+			 Width                                    : 1 920 pixels
+			 Height                                   : 1 080 pixels
+			 Display aspect ratio                     : 16:9
+			 Rotation                                 : 90°
+			 */
+		if (preg_match('/Width.*: ([0-9 ]+) pixels/', $text)) {
+			$pattern = '/Width.*: ([0-9 ]+) pixels/i';
+			$replacement = '${1}';
+			$width = str_replace(' ','',preg_replace($pattern, $replacement, $text));
+		}
+		if (preg_match('/Height.*: ([0-9 ]+) pixels/', $text)) {
+			$pattern = '/Height.*: ([0-9 ]+) pixels/i';
+			$replacement = '${1}';
+			$height = str_replace(' ','',preg_replace($pattern, $replacement, $text));
+		}
+		if (preg_match('/Rotation.*: 90.*/', $text)) {
+			$scale = 'scale=-2:'.$max.',setsar=1:1';
+			$rotated = true;
+		}
+	}
 	if(extension_loaded('ffmpeg')) {
 		$ffmpegInstance = new ffmpeg_movie($input);
 		if ($ffmpegInstance) {
@@ -17,22 +41,28 @@ if (isset($_REQUEST['input'])) {
 			$aspect = $ffmpegInstance->getPixelAspectRatio();
 			$length = $ffmpegInstance->getDuration();
 			if ($length) $duration = $length;
-			if ($height && $width) {
-				if ($width > $height) {
-					// landscape
-					if (!$aspect) $aspect = 1;
-					$scaledwidth = min($max, $width);
-					$scalefactor = $scaledwidth / $width;
-					$scaledheight = round($scalefactor * $height / $aspect);
-					$scale = 'scale='.$scaledwidth.':'.$scaledheight.',setsar=1:1';
-				} else {
-					// portrait
-					$scaledheight = min($max, $height);
-					$scalefactor = $scaledheight / $height;
-					$scaledwidth = $scalefactor * $width;
-					$scale = 'scale='.$scaledwidth.':'.$scaledheight.',setsar=1:1';
-				}
-			}
+		}
+	}
+	if ($height && $width) {
+		if ($rotated) {
+			$tmp = $height;
+			$height = $width;
+			$width = $tmp;
+			unset($tmp);
+		}
+		if ($width > $height) {
+			// landscape
+			if (!$aspect) $aspect = 1;
+			$scaledwidth = min($max, $width);
+			$scalefactor = $scaledwidth / $width;
+			$scaledheight = round($scalefactor * $height / $aspect);
+			$scale = 'scale='.$scaledwidth.':-2,setsar=1:1';
+		} else {
+			// portrait
+			$scaledheight = min($max, $height);
+			$scalefactor = $scaledheight / $height;
+			$scaledwidth = $scalefactor * $width;
+			$scale = 'scale=-2:'.$scaledheight.',setsar=1:1';
 		}
 	}
 	if (file_exists($input)) {
