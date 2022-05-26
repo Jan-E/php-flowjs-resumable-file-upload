f46defd869fa80a1fa4bfaf03f30d69b310cbd54
Jan Ehrhardt <github@ehrhardt.nl>
2022-05-26 18:40:49 +0200

    Add vendor/flowjs/flow-php-server to repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bb15f435094a20aa903310857407e30f4654549f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bb15f435094a20aa903310857407e30f4654549f',
+            'dev_requirement' => false,
+        ),
+        'flowjs/flow-php-server' => array(
+            'pretty_version' => 'v1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../flowjs/flow-php-server',
+            'aliases' => array(),
+            'reference' => 'c6802bf7c68fe8d1e19b60e484bf08f838861728',
+            'dev_requirement' => false,
+        ),
+    ),
+);
